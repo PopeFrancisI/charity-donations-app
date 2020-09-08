0517dfbe49e1cbfa2cccce70e56ba0c7ab570b0b
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-09-08 13:34:41 +0200

    count_bags method

--- donations/views.py
+++ donations/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 
+from donations.models import Donation
+
 
 class LandingPage(View):
 
+    @staticmethod
+    def count_bags():
+        bags_count = Donation.objects.all().aggregate(sum('quantity'))
+        return bags_count
+
     def get(self, request):
-        return render(request, 'index.html')
+        context = {}
+        context['bags_count'] = self.count_bags()
+        context['supported_organisations_count'] = count_supported_organisations()
+
+        return render(request, 'index.html', context)
 
 
 class AddDonation(View):
