c98a077a8f19a4be219fe85f927b300a45efc0d5
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2020-11-16 16:47:47 +0100

    deleted 'donations' from static files urls

--- charityDonationsApp/settings.py
+++ charityDonationsApp/settings.py
@@ -14,7 +14,7 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'donations/static'),
+    os.path.join(BASE_DIR, 'donations/../static'),
 )
 
 LOGIN_URL = '/login/'
